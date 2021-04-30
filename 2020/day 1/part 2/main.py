561fe5c42597061e2f91d3cbb66bbe77ec77b5c8
KoleckOLP-work <o.kurz@atlas.cz>
2021-04-30 13:59:47 +0200

    Writing story of d1p1 and checking d1p2 I found my code hard to read so I rewrtote the code to words. And in Day 1, Part 2 I wondered how many times I run torught the numbers, and it's looping 34 times trought all the 200 numbers.

--- 2020/day 1/part 2/main.py
+++ 2020/day 1/part 2/main.py
@@ -7,13 +7,12 @@ numbers = []
 for line in lines:
     numbers = numbers+[int(line[:-1])]
 
-for numb1 in numbers:
+for i, numb1 in enumerate(numbers):
     for numb2 in numbers:
         wanted = 2020 - (numb1 + numb2)
         if wanted in numbers:
             test = numb1 + numb2 + wanted
             result = numb1 * numb2 * wanted
-            print(f"{numb1}, {numb2}, {wanted}, {str(test)}, {str(result)}")
+            print(f"{numb1}, {numb2}, {wanted}, {str(test)}, {str(result)}, {i}, {len(numbers)}")
             exit()
-
 f.close()
